cdc637400b2c820ad2be4591508a3d41e5fb37a9
burakmahim <burakmahim718@gmail.com>
2024-11-06 11:20:19 +0300

    Proje dosyası ekle.

---
+++ ListPratik/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ziyaretçi isimlerini tutmak için bir liste oluştur
+List<string> ziyaretciListesi = new List<string>();
+
+// Ziyaretçilerin isimlerini listeye ekle
+ziyaretciListesi.Add("Büülent Ersoy");
+ziyaretciListesi.Add("Ajda Pekkan");
+ziyaretciListesi.Add("Ebru Gündeş");
+ziyaretciListesi.Add("Hadise");
+ziyaretciListesi.Add("Hande Yener");
+ziyaretciListesi.Add("Tarkan");
+ziyaretciListesi.Add("Funda Arar");
+ziyaretciListesi.Add("Demet Akalın");
+
+// foreach döngüsü ile liste içerisindeki isimleri ekrana yazdır
+Console.WriteLine("Ziyaretçi Listesi:");
+foreach (string isim in ziyaretciListesi)
+{
+    Console.WriteLine(isim);
+}
